2f32b215037afbcf906089e2e3d5c8c083289dae
mohamadzakaria4499-hue <mohamadzakaria4499@gmail.com>
2025-09-18 14:22:53 +0300

    commiting the top bar

--- src/Components/AppLayout.tsx
+++ src/Components/AppLayout.tsx
@@ -1,34 +1,33 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import TopBar from "./TopBar";
+import TopBar from "./topbar/TopBar";
 import { useState } from "react";
 
-
-
 function AppLayout() {
-    const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
+  return (
+    <>
+      <div
+        className={`grid  grid-rows-[auto_1fr] h-screen `}
+        style={{
+          gridTemplateColumns: isCollapsed ? "60px 1fr" : "26rem 1fr",
+        }}
+      >
+        <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+        <TopBar
+          userName="Mohamad Zakaria"
+          companyName="Partisan Protective Services"
+          userAvatarUrl="https://columbus.in.us/wp-content/uploads/2020/01/person-01.jpg"
+        />
 
-    return (
-
-        <>
-            <div className={`grid  grid-rows-[auto_1fr] h-screen `} style={{
-                gridTemplateColumns: isCollapsed ? "60px 1fr" : "26rem 1fr",
-            }}>
-                <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-                <TopBar />
-
-
-                <main className="bg-blue-200 px-[4.8rem] pt-[4rem] pb-[6.4rem] overflow-auto">
-                    <div className="col-span-1 row-span-1 max-w-[120rem] mx-auto flex flex-col gap-[3.2rem]" >
-                        <Outlet />
-                    </div>
-                </main>
-
-            </div>
-
-        </>
-    )
-
+        <main className="bg-blue-200 rounded-lg px-[4.8rem] pt-[4rem] pb-[6.4rem] overflow-auto">
+          <div className="col-span-1 row-span-1 max-w-[120rem] mx-auto flex flex-col gap-[3.2rem]">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
 export default AppLayout;
