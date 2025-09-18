c61a77a7bf94f84f392e4910d078d2829d68fa82
Abdallah-Wehbe <abdallah62002@gmail.com>
2025-09-18 14:39:50 +0300

    some nonesense changes

--- src/Components/AppLayout.tsx
+++ src/Components/AppLayout.tsx
@@ -3,32 +3,28 @@ import Sidebar from "./Sidebar";
 import TopBar from "./TopBar";
 import { useState } from "react";
 
-
-
 function AppLayout() {
-    const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
+  return (
+    <>
+      <div
+        className={`grid  grid-rows-[auto_1fr] h-screen `}
+        style={{
+          gridTemplateColumns: isCollapsed ? "60px 1fr" : "26rem 1fr",
+          transition: "all 350ms ease-in-out",
+        }}
+      >
+        <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+        <TopBar />
 
-    return (
-
-        <>
-            <div className={`grid  grid-rows-[auto_1fr] h-screen `} style={{
-                gridTemplateColumns: isCollapsed ? "60px 1fr" : "26rem 1fr",
-            }}>
-                <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-                <TopBar />
-
-
-                <main className="bg-blue-200 px-[4.8rem] pt-[4rem] pb-[6.4rem] overflow-auto">
-                    <div className="col-span-1 row-span-1 max-w-[120rem] mx-auto flex flex-col gap-[3.2rem]" >
-                        <Outlet />
-                    </div>
-                </main>
-
-            </div>
-
-        </>
-    )
-
+        <main className="bg-blue-200 px-[4.8rem] pt-[4rem] pb-[6.4rem] overflow-auto">
+          <div className="col-span-1 row-span-1 max-w-[120rem] mx-auto flex flex-col gap-[3.2rem]">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
 export default AppLayout;
