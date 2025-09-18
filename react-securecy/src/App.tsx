e1cb1f865f55b414f8d43fea6158457d705e3e63
mohamadzakaria4499-hue <mohamadzakaria4499@gmail.com>
2025-09-18 13:35:48 +0300

    tt

--- react-securecy/src/App.tsx
+++ react-securecy/src/App.tsx
@@ -1,43 +1,16 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import TopBar from "./components/TopBar"; //
 
 function App() {
-  const [count, setCount] = useState(0)
-  const hello = "helo";
-
-
-  useEffect(() => {
-
-    console.log(count)
-
-  }, []);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <TopBar
+        userName="Mohamad Zakaria"
+        companyName="Partisan Protective Services"
+        userAvatarUrl="https://columbus.in.us/wp-content/uploads/2020/01/person-01.jpg"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
