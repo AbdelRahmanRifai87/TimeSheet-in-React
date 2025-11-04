1d99c9510f2b6d059d571cf1b839e5caf86ac7c1
Abdel Rahman <abouderifai51@gmail.com>
2025-11-04 10:48:27 +0200

    dd

--- src/Components/AppLayout.tsx
+++ src/Components/AppLayout.tsx
@@ -36,7 +36,7 @@ function AppLayout() {
       />
 
       <main
-        className={`  px-5 pt-1 col-span-1 row-span-1 rounded-tl-lg  min-h-[calc(93.5vh + 1px)] outline outline-black overflow-auto transition-colors duration-500 ${
+        className={`  col-span-1 row-span-1   min-h-[calc(93.5vh + 1px)] outline outline-black overflow-auto transition-colors duration-500 ${
           effectiveTheme === "dark" || effectiveTheme === "night"
             ? "bg-[#0d0d0df3] text-white"
             : "bg-[#F1F3F3] text-black"
